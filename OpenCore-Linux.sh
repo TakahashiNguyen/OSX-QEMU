e3879723415c5cb09eba10a6897eadf02177cd81
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-12-17 16:11:41 +0700

    create 1 file, delete 6 files and rename 2 files

--- OpenCore-Linux.sh
+++ OpenCore-Linux.sh
@@ -24,9 +24,6 @@ MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 # macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6.
 
 ALLOCATED_RAM="8192" # MiB
-CPU_SOCKETS="1"
-CPU_CORES="1"
-CPU_THREADS="1"
 
 REPO_PATH="."
 OVMF_DIR="."
@@ -36,41 +33,27 @@ args=(
   -enable-kvm 
   # -accel whpx
   -machine q35
-  -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+  -cpu Penryn,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+  -smp 8,cores=8,sockets=1
   -m "$ALLOCATED_RAM" 
-  # -device nec-usb-xhci,id=xhci -device usb-tablet -device usb-kbd -global nec-usb-xhci.msi=off
-  # -audiodev dsound,id=snd0 -device ich9-intel-hda -device hda-duplex,audiodev=snd0
   -device qemu-xhci,id=xhci
-  -device usb-kbd,bus=xhci.0 -device usb-tablet,bus=xhci.0
-  # -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
-  # -device usb-ehci,id=ehci
-  # -device usb-kbd,bus=ehci.0
-  # -device usb-mouse,bus=ehci.0
-  # -device nec-usb-xhci,id=xhci
-  # -global nec-usb-xhci.msi=off
-  # -global ICH9-LPC.acpi-pci-hotplug-with-bridge-support=off
-  -device usb-host,vendorid=0x8086,productid=0x0808  # 2 USD USB Sound Card
-  # -device usb-host,vendorid=0x1b3f,productid=0x2008  # Another 2 USD USB Sound Card
-  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-  # -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_combined.fd"
-  # -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
-  # -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1920x1080.fd"
-  -smbios type=2
+  -device ich9-intel-hda -device hda-duplex
   -device ich9-ahci,id=sata
+  -device usb-kbd,bus=xhci.0 -device usb-tablet,bus=xhci.0
+  -device usb-ehci,id=ehci
+  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+  -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
+  -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1920x1080.fd"
+  -smbios type=2
   -device ide-hd,bus=sata.2,drive=OpenCoreBoot
   -device ide-hd,bus=sata.3,drive=InstallMedia
   -device ide-hd,bus=sata.4,drive=MacHDD
   -drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore/OpenCore.qcow2"
   -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
   -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
-  -rtc base=localtime -bios "OVMF_combined.fd"
-  # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  # -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  -netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:11:22:33
-  # -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27  # Note: Use this line for High Sierra
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   -monitor stdio
   -device vmware-svga
-  # -spice port=5900,addr=127.0.0.1,disable-ticketing=on
 )
 
 qemu-system-x86_64 "${args[@]}"
---
+++ OpenCore-Windows.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Special thanks to:
+# https://github.com/Leoyzen/KVM-Opencore
+# https://github.com/thenickdude/KVM-Opencore/
+# https://github.com/qemu/qemu/blob/master/docs/usb2.txt
+#
+# qemu-img create -f qcow2 mac_hdd_ng.img 128G
+#
+# echo 1 > /sys/module/kvm/parameters/ignore_msrs (this is required)
+
+###############################################################################
+# NOTE: Tweak the "MY_OPTIONS" line in case you are having booting problems!
+###############################################################################
+#
+# Change `Penryn` to `Haswell-noTSX` in OpenCore-Boot.sh file for macOS Sonoma!
+#
+###############################################################################
+
+
+MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
+
+# This script works for Big Sur, Catalina, Mojave, and High Sierra. Tested with
+# macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6.
+
+ALLOCATED_RAM="8192" # MiB
+
+REPO_PATH="."
+OVMF_DIR="."
+
+# shellcheck disable=SC2054
+args=(
+  # -enable-kvm 
+  -accel whpx
+  -machine q35
+  -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+  -smp 8,cores=8,sockets=1
+  -m "$ALLOCATED_RAM" 
+  -device qemu-xhci,id=xhci
+  -device ich9-intel-hda -device hda-duplex
+  -device ich9-ahci,id=sata
+  -device usb-kbd,bus=xhci.0 -device usb-tablet,bus=xhci.0
+  -device usb-ehci,id=ehci
+  -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+  -bios "OVMF_combined.fd"
+  -smbios type=2
+  -device ide-hd,bus=sata.2,drive=OpenCoreBoot
+  -device ide-hd,bus=sata.3,drive=InstallMedia
+  -device ide-hd,bus=sata.4,drive=MacHDD
+  -drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore/OpenCore.qcow2"
+  -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
+  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  -monitor stdio
+  -device vmware-svga
+)
+
+qemu-system-x86_64.exe "${args[@]}"
